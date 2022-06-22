6acd6bfbaecd188492b2526c3388b0186773cef7
TahaLechgar <tahamr08@gmail.com>
2022-06-22 14:08:45 +0100

    add copied feedback message after copying a link from linkModal

--- src/components/modals/LinkModal.vue
+++ src/components/modals/LinkModal.vue
@@ -3,11 +3,18 @@
         <div class="Modal__contentWrapper bg-white dark:bg-gray-900">
           <div class="linkModal">
             <h3 class="linkModal__title dark:text-white">{{ title }}</h3>
-            <p class="linkModal__description dark:text-white">{{ description }}</p>
+<!--            <p class="linkModal__description dark:text-white">{{ description }}</p>-->
             <div class="linkModal__linkWrapper">
               <input type="text" class="linkModal__link text-gray-700 dark:text-white bg-white dark:bg-slate-700 border-2 border-gray-400 dark:border-slate-600" name="link" :value="link">
-              <button class="linkModal__button linkModal__button--copy" @click="handleClick">Copy</button>
-
+              <div class="relative">
+                <button class="linkModal__button linkModal__button--copy" @click="handleClick">Copy</button>
+                <div v-if="copiedFeedback" class="absolute bottom-8 right-0 bg-purple-100 rounded-lg py-2 px-2.5 mb-3 text-purple-700 inline-flex items-center " role="alert">
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+                  </svg>
+                  Copied
+                </div>
+              </div>
             </div>
             <div class="linkModal__note bg-purple-100 bg-opacity-70 dark:text-white dark:bg-purple-800 dark:bg-opacity-20">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur eaque exercitationem iure iusto laudantium molestiae mollitia quos. Adipisci beatae consequuntur numquam officia quam quasi qui suscipit tempora voluptas voluptatum.
@@ -24,6 +31,7 @@
 <script setup>
   import Modal from "../layouts/Modal.vue"
   import copyTextToClipboard from "../../js/copyToClipboard"
+  import {ref} from "vue";
 
 
   const props = defineProps({
@@ -34,8 +42,12 @@
 
   defineEmits(["close"])
 
+  const copiedFeedback = ref(false)
+
   const handleClick = () => {
     copyTextToClipboard(props.link)
+    copiedFeedback.value = true
+    setTimeout(() => copiedFeedback.value = false, 2000)
   }
 
 </script>
@@ -79,12 +91,11 @@
 
   &__button {
     color: white;
-    padding: 0.5rem 1rem;
+    padding: 0.5rem 2rem;
     font-size: 0.9rem;
     border-radius: 5px;
     &--copy {
       background-color: base.$main;
-      padding: 0.5rem;
     }
   }
 
